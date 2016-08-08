94f18b5665f463b892c7ece8175e40337978272e
Alexandra Zhzhonova <aleksandrazhzhonova@gmail.com>
2016-08-09 00:58:40 +0300

    сделала задание 3 целиком, забыла комитить в процессе

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,2 +1,13 @@
 class Card < ActiveRecord::Base
+  before_create :add_days
+  validates :original_text, :translated_text, presence: true
+  validate :check_word_and_translation
+
+  def check_word_and_translation
+    errors.add(:original_text, "Слово и перевод должны отличаться") if original_text.mb_chars.lstrip.downcase == translated_text.mb_chars.lstrip.downcase
+  end
+
+  def add_days
+    self.review_date = Date.today + 3.days
+  end
 end
